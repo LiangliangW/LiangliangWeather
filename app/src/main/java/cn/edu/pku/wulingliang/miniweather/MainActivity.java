48cca17c311ea9063d166f5201e31142829774ed
Wuliangliang <wuliangliang@pku.edu.cn>
2017-09-26 12:36:17 +0800

    First commit

---
+++ app/src/main/java/cn/edu/pku/wulingliang/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.wulingliang.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by WLL on 2017/9/20.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
